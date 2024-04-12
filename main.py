19ac8616744492d87dfa6ef2b53c86b28b9e9d60
Mohammad Parvari <103634638+mhmdParvari@users.noreply.github.com>
2024-04-13 02:43:17 +0330

    fix minor bug

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import recognizer
 app = FastAPI()
 
 @app.post('/')
-def recognize_face(input: UploadFile = File(None)):
+async def recognize_face(input: UploadFile = File(None)):
     if not input.content_type.startswith('image/'):
         raise HTTPException(415, 'File type not supported')
 
-    content = input.read()
+    content = await input.read()
     np_arr = np.frombuffer(content)
     img = cv.imdecode(np_arr, cv.IMREAD_UNCHANGED)
     cv.imwrite('image.jpg', img)
-    celeb = recognizer.Recognize('image.jpg')
+    celeb = await recognizer.Recognize('image.jpg')
     return {'celebrity': celeb}
